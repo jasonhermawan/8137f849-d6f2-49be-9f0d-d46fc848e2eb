5e2e718ca40cd07894e5b4277ae5d2a3860431f9
jasonhermawan <jasonhermawans@gmail.com>
2024-04-29 17:31:27 +0700

    feat: initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from '@mantine/core';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <Center maw='100vw' h='100vh'>
+      <Text>Table</Text>
+    </Center>
+  );
+};
+
+export default Home;
